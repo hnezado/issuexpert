ebbe068eea54706bc99d117ca7fa2fc466af0323
hnezado <hnezado@gmail.com>
2026-03-30 23:00:24 +0200

    Add create user endpoint (POST /api/users). Tested OK with postman

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
 const userModel = require("../models/userModel");
 
-// GET /users
 async function getAllUsers(req, res) {
   try {
     const users = await userModel.getAllUsers();
@@ -10,6 +9,31 @@ async function getAllUsers(req, res) {
   }
 }
 
+async function createUser(req, res) {
+  try {
+    const { username, email, password, role_id } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+
+    const result = await userModel.createUser(
+      username,
+      email,
+      password,
+      role_id || 3,
+    );
+
+    res.status(201).json({
+      message: "User created",
+      id: result.insertId,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
 module.exports = {
   getAllUsers,
+  createUser,
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -7,6 +7,18 @@ async function getAllUsers() {
   return rows;
 }
 
+async function createUser(username, email, password, role_id = 3) {
+  const sql = `
+        INSERT INTO Users (username, email, password, role_id)
+        VALUES (?, ?, ?, ?)
+    `;
+
+  const [result] = await db.execute(sql, [username, email, password, role_id]);
+
+  return result;
+}
+
 module.exports = {
   getAllUsers,
+  createUser,
 };
