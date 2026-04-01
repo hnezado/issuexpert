9694d74d3f3731c4b8bb89cd5fd175c44d92c20b
hnezado <hnezado@gmail.com>
2026-04-01 19:36:53 +0200

    Feat(b): Updated static files serving path.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // Route mounting
 app.use("/api/users", userRoutes);
