d2efabf59df291cc868f68452fdab9fa965adc99
hnezado <hnezado@gmail.com>
2026-04-20 01:05:37 +0200

    Feat(f): Retrieve logged user data and show on dashboard.

---
+++ public/js/auth/user.js
@@ -0,0 +1,23 @@
+export async function loadCurrentUser() {
+  const token = localStorage.getItem("auth_token");
+
+  try {
+    const res = await fetch(`${API_BASE_URL}/auth/get-user-info`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res.ok) {
+      console.error("Error retrieving user data");
+      return null;
+    }
+
+    const data = await res.json();
+    return data.user;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,30 @@
-const username = "Ataulfo Gerfídelo";
-// const username = "Hector Martinez";
+import { loadCurrentUser } from "./auth/user.js";
 
-const userAvatarContent = Array.from(
-  username.split(" ").map((w) => w[0].toUpperCase()),
-).join("");
+let currentUser = null;
+let userAvatarContent = null;
 
-document.getElementById("user-avatar-content").textContent = userAvatarContent;
+async function init() {
+  currentUser = await loadCurrentUser();
+  userAvatarContent = getUserAvatarContent();
+
+  document.getElementById("user-avatar-content").textContent =
+    userAvatarContent;
+  document.getElementById("userName").textContent =
+    currentUser.username.toUpperCase()[0] + currentUser.username.slice(1);
+  document.getElementById("userRole").textContent = currentUser.role;
+}
+
+function getUserAvatarContent() {
+  try {
+    const username = currentUser.username;
+    // const username = "Ataulfo Gerfídelo";
+    // const username = "Hector Martinez";
+    return Array.from(username.split(" ").map((w) => w[0].toUpperCase())).join(
+      "",
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+init();
