c62029b567b1a10bf193078072da46543736acfc
hnezado <hnezado@gmail.com>
2026-05-15 23:01:48 +0200

    Fix(f): Now formatText() filters out empty strings in the array.

--- public/utils/general.js
+++ public/utils/general.js
@@ -1,10 +1,12 @@
 // Format text capitalizing every word
 function formatText(text = "") {
-  return text
+  const formatted = text
     .trim()
     .split(/\s+/) // One or more spaces
+    .filter((w) => Boolean(w)) // Avoid empty spaces
     .map((w) => w[0].toUpperCase() + w.slice(1).toLowerCase())
     .join(" ");
+  return formatted;
 }
 
 export { formatText };
